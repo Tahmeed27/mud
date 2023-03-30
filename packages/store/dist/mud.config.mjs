95e2253967548d782a2e636400b350db928424bc
straightupjac <jdc.jaclyn@gmail.com>
2023-03-30 16:35:39 -0400

    feat: add yarn docker file

---
+++ packages/store/dist/mud.config.mjs
@@ -0,0 +1,36 @@
+import { storeConfig } from "@latticexyz/cli";
+var mud_config_default = storeConfig({
+  storeImportPath: "../",
+  namespace: "mudstore",
+  tables: {
+    Hooks: "address[]",
+    Callbacks: "bytes24[]",
+    StoreMetadata: {
+      primaryKeys: {
+        tableId: "uint256"
+      },
+      schema: {
+        tableName: "string",
+        abiEncodedFieldNames: "bytes"
+      },
+      storeArgument: true
+    },
+    Mixed: {
+      schema: {
+        u32: "uint32",
+        u128: "uint128",
+        a32: "uint32[]",
+        s: "string"
+      }
+    },
+    Vector2: {
+      schema: {
+        x: "uint32",
+        y: "uint32"
+      }
+    }
+  }
+});
+export {
+  mud_config_default as default
+};
